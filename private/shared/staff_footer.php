8cbb059de65d7a0406283adb6cf7ba9dcb363de7
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-04-21 22:38:44 +0300

    Start up of the project

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,6 @@
+
+    <footer>
+    &copy; <?php echo date('Y'); ?> Globe bank international
+    </footer>
+</body>
+</html>
---
+++ private/shared/staff_header.php
@@ -0,0 +1,26 @@
+<?php
+    if(!isset($page_title)){
+        $page_title="Staff Area";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GBI - <?php echo $page_title ?></title>
+    <link rel="stylesheet" href="../stylesheet/staff.css" media="all">
+</head>
+<body>
+    <header>
+        <h1>GBI staff area</h1>
+    </header>
+
+    <navigation>
+    <ul>
+    <li><a href="index.php">Menu</a></li>
+    </ul>
+    </navigation>
